af9f7c9824d982d026a1836bcfb7f537eefa2cb7
LucasMMF <mateusmidao@gmail.com>
2024-03-03 19:50:48 -0300

    feat: including basic structure and entity
    includes the basic structure for inputs and
    the funcionario entity.

--- Aula01Exercicio/Program.cs
+++ Aula01Exercicio/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Aula01Exercicio.Entities;
+
+namespace Aula01Exercicio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lista de Exercícios 1 - C# WebDeveloper");
+            Console.WriteLine("\n *** CADASTRO DE FUNCIONÁRIOS *** \n");
+
+            var funcionario = new Funcionario();
+
+            try
+            {
+                funcionario.IdFuncionario = Guid.NewGuid();
+
+                Console.Write("Informe o nome do funcionário...............: ");
+                funcionario.Nome = Console.ReadLine();
+
+                Console.Write("Informe o CPF do funcionário................: ");
+                funcionario.Cpf = Console.ReadLine();
+
+                Console.Write("Informe a data de admissão do funcionário...: ");
+                funcionario.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Informe a matricula do funcionário..........: ");
+                funcionario.Matricula = Console.ReadLine();
+
+                Console.WriteLine("\nDADOS DO FUNCIONÁRIO: ");
+                Console.WriteLine($"Id do funcionário........: {funcionario.IdFuncionario}");
+                Console.WriteLine($"Nome do funcionário......: {funcionario.Nome}");
+                Console.WriteLine($"CPF do funcionário.......: {funcionario.Cpf}");
+                Console.WriteLine($"Data de Admissão.........: {funcionario.DataAdmissao}");
+                Console.WriteLine($"Matricula................: {funcionario.Matricula}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n Falha ao cadastrar o funcionário! \n");
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
